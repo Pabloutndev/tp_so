b7ff8c13f64fafda4d882215aa00543067baadcf
Pabloutndev <plarocca@frba.utn.edu.ar>
2024-04-12 22:18:33 +0000

    handshake interfaz-memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -9,5 +9,21 @@ int main(int argc, char* argv[]) {
 
 	t_log* logger = iniciar_logger("memoria.log","memoria");
 	log_info(logger,"LOG MEMORIA \n");
-    return 0;
+    // Creamos una conexión SOCKET SERVIDOR IP:PUERTO_MEMORIA
+	int server_fd = crear_socket(logger,SERVER,IP_GENERICA,PUERTO_MEMORIA);
+	log_info(logger, "Servidor listo para recibir al cliente");
+	int cliente_fd = esperar_cliente(logger,server_fd);
+	
+	if (handshake_serv(logger, cliente_fd) == -1) {
+        log_error(logger,"Error en el handshake con el cliente\n");
+        exit(1);
+    }
+
+	close(server_fd);
+
+	//libera_config_memoria(&config);
+	log_info(logger,"Modulo memoria finalizado.");
+	log_destroy(logger);
+	
+	return 0;
 }
